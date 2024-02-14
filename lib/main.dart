a0145ee04c494910330e75c2521d533ce78cec57
navas <gabrielnavas@email.com>
2024-02-14 15:46:24 -0300

    initital version

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Expenses());
+
+class Expenses extends StatelessWidget {
+  const Expenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas pessoais'),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text('Versão inicial'),
+      ),
+    );
+  }
+}
