27796c6390c97f90120c6d18d9afa26c5c5ecaa3
navas <gabrielnavas@email.com>
2024-02-15 14:24:15 -0300

    remove data from widget stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'package:expenses_flutter_app/models/transaction.dart';
-import 'package:expenses_flutter_app/widgets/transaction_form.dart';
-import 'package:expenses_flutter_app/widgets/transaction_list.dart';
 import 'package:expenses_flutter_app/widgets/transaction_ser.dart';
 import 'package:flutter/material.dart';
 
@@ -18,24 +15,6 @@ class Expenses extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> _transactions = [
-    Transaction(
-        id: '1', title: 'Tenis novo', value: 300.50, date: DateTime.now()),
-    Transaction(
-        id: '2', title: 'Camiseta nova', value: 70.50, date: DateTime.now()),
-    Transaction(
-        id: '3', title: 'Mouse novo', value: 150.50, date: DateTime.now()),
-    Transaction(
-        id: '4', title: 'Teclado nova', value: 200.50, date: DateTime.now()),
-    Transaction(
-        id: '5', title: 'Monitor novo', value: 700.50, date: DateTime.now()),
-    Transaction(
-        id: '6',
-        title: 'Ar-condicionado',
-        value: 1500.50,
-        date: DateTime.now()),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
