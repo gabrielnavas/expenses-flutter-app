0094e0dd3aed8639090eb08b598bc597c651eccc
navas <gabrielnavas@email.com>
2024-02-15 15:45:43 -0300

    add submit in number input keyboard and button

--- lib/widgets/transaction_form.dart
+++ lib/widgets/transaction_form.dart
@@ -1,34 +1,52 @@
+import 'dart:math';
+
+import 'package:expenses_flutter_app/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
-  final void Function(String title, double value) onSubmit;
+  final void Function(Transaction transaction) getNewTransaction;
 
-  TransactionForm(this.onSubmit, {super.key});
+  TransactionForm(this.getNewTransaction, {super.key});
+
+  void _onPressedSubmitForm() {
+    Transaction transaction = Transaction(
+        id: Random().nextDouble().toString(),
+        title: titleController.text,
+        value: double.tryParse(valueController.text) ?? 0.00,
+        date: DateTime.now());
+    if (!transaction.valid()) {
+      return;
+    }
+    getNewTransaction(transaction);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
       child: Container(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             TextField(
               controller: titleController,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Título',
               ),
             ),
             TextField(
               controller: valueController,
-              decoration: InputDecoration(
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              onSubmitted: (String a) => _onPressedSubmitForm(),
+              decoration: const InputDecoration(
                 labelText: 'Valor (R\$)',
               ),
             ),
-            Container(
+            SizedBox(
               width: double.infinity,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -37,12 +55,8 @@ class TransactionForm extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                child: Text('Nova transação'),
-                onPressed: () {
-                  String title = titleController.text;
-                  double value = double.tryParse(valueController.text) ?? 0.00;
-                  onSubmit(title, value);
-                },
+                onPressed: _onPressedSubmitForm,
+                child: const Text('Nova transação'),
               ),
             )
           ],
